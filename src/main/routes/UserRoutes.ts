f5e2591688d5c90dec7ec923b8ef73ab87b86c6d
Shashank Baruah <starkviper@gmail.com>
2024-03-07 20:19:19 +0000

    feat: added validation middlewares

--- src/main/routes/UserRoutes.ts
+++ src/main/routes/UserRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getProfile, updateProfile } from "../middlewares/usecases/UserProfile";
+import { updateValidator, validatorMw } from "../middlewares/common/Validators";
 
 const userRoutes = Router();
 
 userRoutes.get("/profile", getProfile);
 
-userRoutes.put("/profile", updateProfile);
+userRoutes.put("/profile", updateValidator, validatorMw, updateProfile);
 
 export default userRoutes;
